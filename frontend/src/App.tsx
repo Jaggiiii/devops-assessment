38f26cf0580fcb7c2fdd15f368d0a16538080dd8
jaggiiii <duppalapudijagadeesh5@gmail.com>
2026-01-28 02:42:36 +0530

    Revert "testing1.1"
    This reverts commit 8fddcde5dfec246c77559cc3bb2a070102bc0235.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,6 @@ function App() {
         <h1>DevOps Assessment</h1>
         <p className="subtitle">Full-stack Hello World Application</p>
         <h2> Testing CI CD</h2>
-        <h3>jaggi</h3>
       </header>
       <main className="main-card">
         <div className="status-badge">
